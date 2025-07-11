4c7f2cd2bc8c069ec3e93e96628a86f08417fbe0
Your Name <dicksonwachira886@gmail.com>
2025-07-11 20:26:01 +0300

    Add CRUD operations for books including POST, PUT and  DELETE

--- app.js
+++ app.js
@@ -36,6 +36,41 @@ app.get('/get/:id', (req, res) => {
     }
 });
 
+//add a new book
+app.post('/add', (req, res) => {
+    const newBook = {
+        id: Math.floor(Math.random() * 1000),
+        title: req.body.title || "Default Title",
+        author: req.body.author || "Unknown Author"
+    };
+    books.push(newBook);
+    res.status(201).json({ message: 'Book added successfully', book: newBook });
+});
+
+// Update a book by ID
+app.put('/update/:id', (req, res) => {
+    const findCurrentBook = books.find(bookItem => bookItem.id === parseInt(req.params.id));
+
+    if (findCurrentBook) {
+        findCurrentBook.title = req.body.title || findCurrentBook.title
+        findCurrentBook.author = req.body.author || findCurrentBook.author
+        res.status(200).json({ message: `Book with ID ${findCurrentBook.id} updated successfully`, book: findCurrentBook });    
+    } else {
+        res.status(404).json({ message: 'Book not found' });
+    }
+});
+
+// Delete a book by ID
+app.delete('/delete/:id', (req, res) => {
+    const findIndexOfCurrentBook = books.findIndex(bookItem => bookItem.id === parseInt(req.params.id));
+    if (findIndexOfCurrentBook !== -1) {
+        const deletedBook = books.splice(findIndexOfCurrentBook, 1);
+        res.status(200).json({ message: `Book with ID ${deletedBook[0].id} deleted successfully` , data: deletedBook[0] });
+    } else {
+        res.status(404).json({ message: 'Book not found' });
+    }
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
